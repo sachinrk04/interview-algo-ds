0d359cac09cd64b957d01b730043559da7e95838
Sachin Singh <sachin.innoventes@phonepe.com>
2021-08-22 01:30:52 +0530

    reverestring

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// Method-1
+
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// Method-2
+
+function reverse(str) {
+    let reversed = '';
+    for (let ch of str) {
+        reversed = ch + reversed;
+    }
+    return reversed;
+}
+
+// Method-3
+
+// function reverse(str) {
+//     // return str.split('').reduce((reversed, ch) => {
+//     //     return ch + reversed;
+//     // }, '');
+//     return str.split('').reduce((reversed, ch) => ch + reversed, '');
+// }
+
+module.exports = reverse;
