58c66233ef5f02f7774cf3a8a843d8ad54ea1e73
Sachin Singh <sachin.innoventes@phonepe.com>
2021-08-24 22:39:59 +0530

    22 is done

---
+++ exercises/5-integer-reversal/index.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+// Method-1
+
+// function reverseInt(n) {
+//     const rev =  n.toString().split('').reverse().join('');
+
+//     if (n < 0) {
+//         return parseInt(rev) * -1
+//     }
+//     return parseInt(rev);
+// }
+
+// Method-2
+
+function reverseInt(n) {
+    const rev =  n.toString().split('').reverse().join('');
+
+    return parseInt(rev) * Math.sign(n);
+}
+
+module.exports = reverseInt;
